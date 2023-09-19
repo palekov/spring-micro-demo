f32f07cccc52f322fff8af295e6c9003bc099605
Palekov Alexander <palekov-2011@mail.ru>
2023-09-19 21:29:11 +0300

    added properties to test load balancer

--- eureka-client/src/main/java/com/palekov/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/palekov/micro/demo/eurekaclient/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.palekov.micro.demo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
+    @Value("${eureka.instance.instance-id}")
+    private String id;
 
     @GetMapping("/test")
     public String test(){
-        return "Microservice test is working!";
+        return "Microservice-" + id + " test is working!";
     }
 
 }
